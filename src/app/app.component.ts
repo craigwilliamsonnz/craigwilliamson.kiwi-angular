c02fdab68c80784cd03c686f7f4f7b102c8dafdc
Craig Williamson <craigtw.online@outlook.com>
2025-06-04 14:58:26 +1200

    Added bootstrap widgets to project.  Bootstrap CSS is still coming in via CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faAnchorCircleCheck} from "@fortawesome/free-solid-svg-icons/faAnchorCircleCheck";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FontAwesomeModule, RouterOutlet, HomeComponent],
+  imports: [FontAwesomeModule, RouterOutlet, HomeComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
