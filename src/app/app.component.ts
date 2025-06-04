76a0a4ea45a6b631d9d1ca1ea9e56f115c567906
Craig Williamson <craigtw.online@outlook.com>
2025-06-04 14:37:10 +1200

    Removed fontawesome package and add it via ng so it is now part of the Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faAnchorCircleCheck} from "@fortawesome/free-solid-svg-icons/faAnchorCircleCheck";
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [FontAwesomeModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'craigwilliamson.kiwi-angular';
+  faCoffee = faCoffee;
+  faAnchorCircleCheck = faAnchorCircleCheck;
 }
